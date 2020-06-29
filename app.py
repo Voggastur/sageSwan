5f623c7cd7efb2b4c29826406325348fe60376f5
Voggastur <v3x0r_x014@hotmail.com>
2020-06-29 14:56:34 +0000

    Hid Secret Key

--- app.py
+++ app.py
@@ -2,10 +2,14 @@ import os
 from flask import Flask, render_template, redirect, request, url_for
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
+from os import path
+if path.exists("env.py"):
+  import env 
 
 app = Flask(__name__)
+SECRET_KEY = os.environ.get('SECRET_KEY')
 app.config["MONGO_DBNAME"] = 'task_manager'
-app.config["MONGO_URI"] = 'mongodb+srv://root:r00tTarantula@myfirstcluster-s0ett.mongodb.net/task_manager?retryWrites=true&w=majority'
+app.config["MONGO_URI"] = 'SECRET_KEY'
 
 mongo = PyMongo(app)
 
